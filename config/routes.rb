913dd309d622fe444d73348df2922398453ec5af
Melvin1Atieno <melvinatieno@gmail.com>
2019-02-04 20:01:48 +0300

    Hello rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # tells rails to map requests to the root to the welcome controllers index action
+  root 'welcome#index'
 end
