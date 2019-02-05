d77027dae564cb0eb16ee071183491a900cc3fac
Melvin1Atieno <melvinatieno@gmail.com>
2019-02-05 13:31:08 +0300

    Create articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
   # tells rails to map requests to the root to the welcome controllers index action
   root 'welcome#index'
 end
