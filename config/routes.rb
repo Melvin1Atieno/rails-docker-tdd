ddef97aa7b8c32c8da179c1612232ad8b181821a
Melvin1Atieno <melvinatieno@gmail.com>
2019-02-05 19:07:25 +0300

    generate comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # tells rails to map requests to the root to the welcome controllers index action
   root 'welcome#index'
 end
